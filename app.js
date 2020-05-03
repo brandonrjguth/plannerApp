da0a27af6b9fd445ef92a8bd5c7ee82918e3ccd5
brandonrjguthrie@gmail.com <brandonrjguthrie@gmail.com>
2020-05-02 22:05:48 -0700

    'commit'

--- app.js
+++ app.js
@@ -1,37 +1,95 @@
 //Requires
 var express = require("express");
-var mongoose = require('mongoose');
 var ejs = require("ejs");
 var bodyParser = require('body-parser');
-var mongodb = require('mongodb');
-
 var app = express();
 var port = 80;
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017';
+const dbName = 'plannerApp';
+const client = new MongoClient(url);
+var db;
+var other;
+var masterList;
+var parentList;
 
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
 
-
-
-
+  //Assign Variables for within scope of MongoClient
+  db = client.db(dbName);
+  var listTypes = db.collection('listTypes');
+  var lists = db.collection('lists');
+  var listItems = db.collection('listItems');
+  var currentPage;
+  var currentList;
+  var wtf;
+  var masterList;
+ 
 
 //ROUTES
 
+//Main Page lists the List Types, allows for addition
 app.get('/', function(req, res) {
-    res.render('index');
+  listTypes.find().toArray(function(err, listTypes) {
+    res.render('index', {listTypes:listTypes});
+  });
 });
 
-app.post('/testButton', function(req, res) {
-    console.log(req.body.input);
+app.get('/index/:listTypes', function(req, res) {
+  
+  wtf = req.params.listTypes;
+  masterList = req.params.listTypes;
+  console.log(wtf);
+  lists.find({wtf: masterList}).toArray(function(err, lists) {
+    console.log(wtf);
+    res.render('lists', {lists:lists, wtf:masterList});
+    
+  }); 
+});
 
+app.get('/index/:listTypes/:lists', function(req, res) {
+    parentList = req.params.lists;
+    wtf = req.params.listTypes;
+  listItems.find({wtf:wtf, parentList:parentList}).toArray(function(err, listItems) {
+    parentList = req.params.lists;
+    res.render('listItems', {listItems:listItems});
+    
+  }); 
 });
 
 
 
+app.post('/listTypes', function (req, res) {
+  listTypes.insertOne({name:req.body.name});
+  res.redirect('/');
+});
 
+app.post('/lists', function (req, res) {
+  lists.insertOne({name:req.body.name, wtf:masterList});
+  res.redirect('/index/' + masterList);
+  
+});
+
+app.post('/listItems', function (req, res) {
+  listItems.insertOne({name:req.body.name, wtf:wtf, parentList:parentList});
+  res.redirect('/index/' + wtf + '/' + parentList);
+});
 
 //Listen on port.
 app.listen(port, function() {
     console.log("Listening on port 80");
 });
+
+
+
+});
+
+
+
+     
